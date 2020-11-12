54c2199bcf74d0a10d614ae2ec7a05b0aa629274
Kristinemalveda1985 <kristinemalveda@yahoo.com>
2020-11-12 10:29:41 +0100

    small changes

--- src/lab2/PersonTest.java
+++ src/lab2/PersonTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+
+@TestInstance(Lifecycle.PER_CLASS)
 public class PersonTest {
 
 	private final Person person = new Person();
-
+	
 	@BeforeAll
 	public void setUp() {
 		person.setFirstName("Kristine");
